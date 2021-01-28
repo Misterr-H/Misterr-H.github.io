25ddd9dafd0af46254ecaa28e86e69c154bc6f5a
Mister-H <55010619+Misterr-H@users.noreply.github.com>
2021-01-28 12:54:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById('nav').style.width="100%";
+}
+
+function closebtn() {
+    document.getElementById('nav').style.width="0%";
+}
+
+function pop() {
+    alert("Backend not done yet. Sorry for inconvenience.");
+}
